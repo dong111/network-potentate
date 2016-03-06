68ddf7b2643ab7e1a3938a24e44c3b3b56ac7a84
陈栋 <chendong@yitaosoft.com>
2016-03-07 07:39:51 +0800

    使用NSNumber 防止服务器端返回null
    使用NSNumber 防止服务器端返回null

--- 16-03-07-(淬火)-XML(Dom)解析/16-03-07-(淬火)-XML(Dom)解析/Video.h
+++ 16-03-07-(淬火)-XML(Dom)解析/16-03-07-(淬火)-XML(Dom)解析/Video.h
@@ -2,6 +2,10 @@
 
 @interface Video : NSObject
 
+
+
+//@property(nonatomic,assign) int videoId;
+// 使用NSNumber 防止服务器端返回null，我们kvc赋值的时候 崩溃
 @property (nonatomic, copy) NSNumber *videoId;
 @property (nonatomic, copy) NSString *name;
 @property (nonatomic, copy) NSNumber *length;
