61246b46645dd9c2d6ef3ac9cb0f2987fc30fce1
陈栋 <chendong@yitaosoft.com>
2016-03-07 07:38:00 +0800

    xml解析逻辑实现
    遍历一二级元素节点  遍历一级元素属性节点

--- 16-03-07-(淬火)-XML(Dom)解析/16-03-07-(淬火)-XML(Dom)解析/ViewController.m
+++ 16-03-07-(淬火)-XML(Dom)解析/16-03-07-(淬火)-XML(Dom)解析/ViewController.m
@@ -8,6 +8,7 @@
 
 #import "ViewController.h"
 #import "Video.h"
+#import "GDataXMLNode.h"
 
 
 @interface ViewController ()
@@ -40,9 +41,32 @@
     [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
         
         //将xml读入内存，生成GDataXMLDocument对象
-        //开始遍历element
+        GDataXMLDocument *document = [[GDataXMLDocument alloc] initWithData:data encoding:NSUTF8StringEncoding error:NULL];
         
-        //开始遍历属性
+        
+        //开始遍历第一级节点element
+        for (GDataXMLElement *ele in document.rootElement.children) {
+//            NSLog(@"-->%@",ele);
+            //构造Video对象
+            Video *video = [[Video alloc] init];
+            //遍历第二级节点
+            for (GDataXMLElement *ele2 in ele.children) {
+                //给对象属性赋值
+                [video setValue:ele2.stringValue forKeyPath:ele2.name];
+            }
+            
+            //遍历第一级元素属性节点
+            for (GDataXMLNode *node in ele.attributes) {
+                NSLog(@"attr -->%@",node);
+                [video setValue:node.stringValue forKeyPath:node.name];
+                
+            }
+            //添加到数组
+            [self.videos addObject:video];
+            
+        }
+        
+        NSLog(@"videos count =%d",self.videos.count);
         
         
     }];
