401547a5db95c49a4f6dffb0ac1923d819ca54df
陈栋 <chendong@yitaosoft.com>
2016-03-07 07:52:32 +0800

    NSDictionary 本地化输出  分离实现
    NSDictionary 本地化输出  分离实现

--- 16-03-07-(淬火)-XML(Dom)解析/16-03-07-(淬火)-XML(Dom)解析/NSArray+Log.m
+++ 16-03-07-(淬火)-XML(Dom)解析/16-03-07-(淬火)-XML(Dom)解析/NSArray+Log.m
@@ -34,3 +34,20 @@
 }
 
 @end
+
+
+
+@implementation NSDictionary (Log)
+
+- (NSString *)descriptionWithLocale:(id)locale
+{
+    NSMutableString *strM = [NSMutableString stringWithString:@"{\n"];
+    
+    [self enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+        [strM appendFormat:@"\t%@ = %@;\n", key, obj];
+    }];
+    
+    [strM appendString:@"}\n"];
+    
+    return strM;
+}
