422ee54aad9e016756bc7d383a50ede0c26c127d
陈栋 <chendong@yitaosoft.com>
2016-03-06 21:57:36 +0800

    （非代码）xml解析项目构建
    （非代码）xml解析项目构建

---
+++ 16-03-06-(淬火)-xml解析/16-03-06-(淬火)-xml解析/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  16-03-06-(淬火)-xml解析
+//
+//  Created by 陈栋 on 16/3/6.
+//  Copyright © 2016年 man. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 16-03-06-(淬火)-xml解析/16-03-06-(淬火)-xml解析/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  16-03-06-(淬火)-xml解析
+//
+//  Created by 陈栋 on 16/3/6.
+//  Copyright © 2016年 man. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
