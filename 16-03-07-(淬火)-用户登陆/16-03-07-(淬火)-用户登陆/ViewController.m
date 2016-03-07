e0a048646d14669306f867b8da384d9f2753a0b6
陈栋 <chendong@yitaosoft.com>
2016-03-07 10:23:54 +0800

    模拟用户登陆  使用post 和get请求
    模拟用户登陆  使用post 和get请求

--- 16-03-07-(淬火)-用户登陆/16-03-07-(淬火)-用户登陆/ViewController.m
+++ 16-03-07-(淬火)-用户登陆/16-03-07-(淬火)-用户登陆/ViewController.m
@@ -16,12 +16,67 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self postLogin];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+- (void) postLogin
+{
+    // 1. url
+    NSString *urlString = @"http://localhost:8080/ios/api/v1/ios/post";
+    
+    NSURL *url = [NSURL URLWithString:urlString];
+    
+    // 2. 可变的请求
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url cachePolicy:1 timeoutInterval:2.0f];
+    //设置请求为Post请求
+    request.HTTPMethod = @"POST";
+    
+    
+    //指定请求数据体body
+    NSString *userName = @"张三";
+    NSString *pwd = @"123";
+    NSString *bodyStr = [NSString stringWithFormat:@"userName=%@sa&pwd=%@",userName,pwd];
+    
+    [request setHTTPBody:[bodyStr dataUsingEncoding:NSUTF8StringEncoding]];
+    
+    //3.连接
+    [NSURLConnection sendAsynchronousRequest:request queue:[[NSOperationQueue alloc] init] completionHandler:^(NSURLResponse * _Nullable response, NSData * _Nullable data, NSError * _Nullable connectionError) {
+        
+        NSString *str = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        NSLog(@"result = %@",str);
+        
+    }];
+    
 }
+
+
+//get 登陆
+- (void) getLogin
+{
+    //准备参数和url
+    NSString *userName = @"张三";
+    NSString *pwd = @"123";
+    NSString *urlString = [NSString stringWithFormat:@"http://localhost:8080/ios/api/v1/ios/login?userName=%@sa&pwd=%@",userName,pwd];
+    //url里面不能包含中文等特殊字符
+    urlString = [urlString stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    
+    NSURL *url = [NSURL URLWithString:urlString];
+    
+    //2,准备请求
+    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:1 timeoutInterval:15];
+    
+
+    //3.连接
+    [NSURLConnection sendAsynchronousRequest:request queue:[[NSOperationQueue alloc] init] completionHandler:^(NSURLResponse * _Nullable response, NSData * _Nullable data, NSError * _Nullable connectionError) {
+        
+        NSString *str = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        NSLog(@"result = %@",str);
+        
+    }];
+    
+}
+
+
 
 @end
